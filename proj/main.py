ab0b950fc96701276ddd1b416ece21f74a6c8af7
Hugo Castro <hreisc@gmail.com>
2024-11-21 15:15:52 +0000

    Changed the visualization of the queue and parking

--- proj/main.py
+++ proj/main.py
@@ -5,19 +5,19 @@ from model import ParkingLotModel
 from portrayal import car_portrayal
 from auxiliar import calculate_dimensions
 
-common_spots = 20
+common_spots = 40
 electric_spots = 0
 premium_spots = 0
 width, height = calculate_dimensions(common_spots, electric_spots, premium_spots)
 # Setting up CanvasGrid and ChartModule for visualization
-grid = CanvasGrid(car_portrayal, 5, 5, 500, 550)  # 20x11 (10 para estacionamento + 1 para fila)
+grid = CanvasGrid(car_portrayal, width, height + 2, 500, 550)  # 20x11 (10 para estacionamento + 1 para fila)
 
 # Launch the simulation in a browser window
 server = ModularServer(
     ParkingLotModel,
     [grid],
     "Parking Lot Model",
-    {"height": height, "width": width,
+    {"height": height + 2, "width": width,
      "common_spots": common_spots, "electric_spots": electric_spots, "premium_spots": premium_spots,
      "electric_chance": 0.4, "premium_chance": 0.1, "starting_cars": 30}
 )
@@ -28,8 +28,9 @@ if __name__ == "__main__":
 
 # Run the simulation
 if __name__ == "__main__":
-    parking_lot = ParkingLotModel(20, 10, 20)
-
+    parking_lot = ParkingLotModel(width, height + 2, common_spots)
+    
     for i in range(10):  # Run for 10 steps
         parking_lot.step()
+        print(parking_lot.grid.width, parking_lot.grid.height)
         print(f"Step {i + 1}: {parking_lot.count_parked_cars()} cars parked.")
--- proj/model.py
+++ proj/model.py
@@ -14,7 +14,7 @@ class ParkingLotModel(Model):
         super().__init__()  # Explicitly initialize the base Model class
         self.datacollector = None
         self.num_agents = starting_cars
-        self.grid = MultiGrid(width, height+1, torus=False)
+        self.grid = MultiGrid(width, height, torus=False)
         self.schedule = RandomActivation(self)
 
         self.common_spots = common_spots
@@ -29,13 +29,50 @@ class ParkingLotModel(Model):
         self.num_spots = 0
         self.aux = 3
             
+            
+    def count_parked_cars(self):
+        return sum([1 for a in self.schedule.agents if isinstance(a, Car) and a.parked])           
 
+    def step(self):
+        self.add_car_to_queue()
+        self.atualize_grid()
+            
+        empty_spots = self.check_empty_spots()
+        
+        if self.aux < 0 :
+            if len(empty_spots) != 0:
+                self.place_car_empty_spot(self.queue[0], empty_spots)
+        
+        self.aux -= 1
+        self.schedule.step()
+                
+    def add_car_to_queue(self):
+        car_type = random.choice(["Normal", "Electric", "Premium"])
+        car = Car(self.car_id, self, car_type)
+        self.queue.append(car)
+        self.car_id += 1
+        
+    def atualize_grid(self):
+        # Place cars in queue grid
+        for x, car in enumerate(self.queue[:self.grid.width]):
+            self.grid.place_agent(car, (x, 0))  # Place car in queue grid cell
+  
+    def check_empty_spots(self):
+        empty_spots = [(x, y) for x in range(self.grid.width) for y in range(2, self.grid.height)
+                           if self.grid.is_cell_empty((x, y))]
+        return empty_spots
+    
     def place_car_empty_spot(self, car, empty_spots):
         spot = empty_spots[0]
         self.grid.place_agent(car, spot)
         self.queue.pop(0)  # Remover o carro da fila
         car.parked = True
-
+        
+    def find_spot(self):
+        return
+        
+        
+    """
     def create_normal_cars(self):
         for i in range(int(self.num_agents * (1 - self.number_electric - self.number_premium))):
             car = Car(i, self, "Normal")
@@ -53,38 +90,4 @@ class ParkingLotModel(Model):
             car = Car(i, self, "Premium")
             self.schedule.add(car)
             self.place_car_random_empty_spot(car)
-            
-    def count_parked_cars(self):
-        return sum([1 for a in self.schedule.agents if isinstance(a, Car) and a.parked])           
-
-    def step(self):
-        self.add_car_to_queue()
-        self.aux -= 1
-        
-        # Place cars in queue grid
-        for x, car in enumerate(self.queue[:self.grid.width]):
-            self.grid.place_agent(car, (x, self.grid.height - 1))  # Place car in queue grid cell
-            
-        empty_spots = self.check_empty_spots()
-        
-        if self.aux < 0 :
-            if len(empty_spots) != 0:
-                self.place_car_empty_spot(self.queue[0], empty_spots)
-        
-        self.schedule.step()
-        
-    def add_car_to_queue(self):
-        car_type = random.choice(["Normal", "Electric", "Premium"])
-        car = Car(self.car_id, self, car_type)
-        self.queue.append(car)
-        self.car_id += 1
-        
-    def check_empty_spots(self):
-        empty_spots = [(x, y) for x in range(self.grid.width) for y in range(self.grid.height - 1)
-                           if self.grid.is_cell_empty((x, y))]
-        
-        return empty_spots
-        
-    def find_spot(self):
-        return
-        
+    """
